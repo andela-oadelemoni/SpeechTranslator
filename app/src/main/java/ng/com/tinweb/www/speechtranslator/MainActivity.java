99b12c501894315517738a2c4b1d86eeedb78567
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-08-02 10:10:03 +0100

    Initial Commit

---
+++ app/src/main/java/ng/com/tinweb/www/speechtranslator/MainActivity.java
@@ -0,0 +1,18 @@
+package ng.com.tinweb.www.speechtranslator;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ng.com.tinweb.www.speechtranslator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
