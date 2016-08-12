b99cdd823511746958a237551bba61b2a271fcb3
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2016-08-13 00:12:43 +0100

    Add subscription to notify user of current status

--- app/src/main/java/ng/com/tinweb/www/speechtranslator/MainActivity.java
+++ app/src/main/java/ng/com/tinweb/www/speechtranslator/MainActivity.java
@@ -19,13 +19,18 @@ import android.widget.Toast;
 
 import com.pubnub.api.PubNub;
 import com.pubnub.api.callbacks.PNCallback;
+import com.pubnub.api.callbacks.SubscribeCallback;
 import com.pubnub.api.models.consumer.PNPublishResult;
 import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import ng.com.tinweb.www.speechtranslator.databinding.ActivityMainBinding;
 
@@ -34,10 +39,14 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
     private static final int PERMISSION_REQUEST_CODE = 7;
 
+    private static final String JSON_KEY = "command";
+
     private SpeechRecognizer speechRecognizer;
     private SpeechRecognitionManager recognitionManager;
     private Intent recognizerIntent;
     private String LOG_TAG = "Voice";
+
+    private PubNub pubNub;
 
     private ActivityMainBinding binding;
     private boolean isKeyPressed = false;
@@ -60,6 +69,10 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
         // RECOGNIZER INTENTS
         setupRecognizerIntent();
+
+        // set pubnub object;
+        pubNub = SpeechTranslatorApplication.getPubNubObject();
+        subscribeToPubNub();
     }
 
     private void checkAudioPermission() {
@@ -166,29 +179,28 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
         String decodedText = text.toLowerCase();
         JSONObject jsonObject = new JSONObject();
         if (decodedText.matches("(.*)bulb(.*)off(.*)")) {
-            jsonObject.put("command", "bulb off");
+            jsonObject.put(JSON_KEY, "bulb off");
         }
         else if (decodedText.matches("(.*)bulb(.*)on(.*)")) {
-            jsonObject.put("command", "bulb on");
+            jsonObject.put(JSON_KEY, "bulb on");
         }
         else if (decodedText.matches("(.*)fan(.*)off(.*)")) {
-            jsonObject.put("command", "fan off");
+            jsonObject.put(JSON_KEY, "fan off");
         }
         else if (decodedText.matches("(.*)fan(.*)on(.*)")) {
-            jsonObject.put("command", "fan on");
+            jsonObject.put(JSON_KEY, "fan on");
         }
         else {
-            jsonObject.put("command", "invalid command received");
+            jsonObject.put(JSON_KEY, "invalid command received");
         }
         publishMessage(jsonObject);
     }
 
     private void publishMessage(JSONObject message) throws JSONException {
         if (message.length() > 0) {
-            PubNub pubNub = SpeechTranslatorApplication.getPubNub();
             pubNub.publish()
-                    .message(message.get("command"))
-                    .channel("Channel=IOTHack")
+                    .message(message.get(JSON_KEY))
+                    .channel(SpeechTranslatorApplication.getPubnubChannel())
                     .shouldStore(true)
                     .usePOST(true)
                     .async(new PNCallback<PNPublishResult>() {
@@ -205,6 +217,89 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
                         }
                     });
+        }
+    }
+
+    private void subscribeToPubNub() {
+        SubscribeCallback subscribeCallback = new SubscribeCallback() {
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+                // the status object returned is always related to subscribe but could contain
+                // information about subscribe, heartbeat, or errors
+                // use the operationType to switch on different options
+                switch (status.getOperation()) {
+                    // let's combine unsubscribe and subscribe handling for ease of use
+                    case PNSubscribeOperation:
+                    case PNUnsubscribeOperation:
+                        // note: subscribe statuses never have traditional
+                        // errors, they just have categories to represent the
+                        // different issues or successes that occur as part of subscribe
+                        switch(status.getCategory()) {
+                            case PNConnectedCategory:
+                                // this is expected for a subscribe, this means there is no error or issue whatsoever
+                            case PNReconnectedCategory:
+                                // this usually occurs if subscribe temporarily fails but reconnects. This means
+                                // there was an error but there is no longer any issue
+                            case PNDisconnectedCategory:
+                                // this is the expected category for an unsubscribe. This means there
+                                // was no error in unsubscribing from everything
+                            case PNUnexpectedDisconnectCategory:
+                                // this is usually an issue with the internet connection, this is an error, handle appropriately
+                                // retry will be called automatically
+                            case PNAccessDeniedCategory:
+                                // this means that PAM does allow this client to subscribe to this
+                                // channel and channel group configuration. This is another explicit error
+                            default:
+                                // More errors can be directly specified by creating explicit cases for other
+                                // error categories of `PNStatusCategory` such as `PNTimeoutCategory` or `PNMalformedFilterExpressionCategory` or `PNDecryptionErrorCategory`
+                        }
+
+                    case PNHeartbeatOperation:
+                        // heartbeat operations can in fact have errors, so it is important to check first for an error.
+                        // For more information on how to configure heartbeat notifications through the status
+                        // PNObjectEventListener callback, consult <link to the PNCONFIGURATION heartbeart config>
+                        if (status.isError()) {
+                            // There was an error with the heartbeat operation, handle here
+                        } else {
+                            // heartbeat operation was successful
+                        }
+                    default: {
+                        // Encountered unknown status type
+                    }
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                final String status = message.getMessage().get(JSON_KEY).textValue();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showReturnMessage(status);
+                    }
+                });
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        };
+        pubNub.addListener(subscribeCallback);
+
+        List<String> channels = new ArrayList<>();
+        channels.add(SpeechTranslatorApplication.getPubnubChannel());
+        pubNub.subscribe()
+                .channels(channels)
+                .execute();
+    }
+
+    private void showReturnMessage(String message) {
+        String[] returnMessages = getResources().getStringArray(R.array.return_messages);
+        List<String> messagesList = new ArrayList<>(Arrays.asList(returnMessages));
+
+        if (messagesList.contains(message)) {
+            binding.status.setText(message);
         }
     }
 
--- app/src/main/java/ng/com/tinweb/www/speechtranslator/SpeechTranslatorApplication.java
+++ app/src/main/java/ng/com/tinweb/www/speechtranslator/SpeechTranslatorApplication.java
@@ -10,20 +10,34 @@ import com.pubnub.api.PubNub;
  */
 public class SpeechTranslatorApplication extends Application {
 
+    private static final String PUBNUB_CHANNEL = "IOTHack";
+    private static final String PUBNUB_SUB_KEY = "sub-c-2e7ff474-5863-11e5-b018-0619f8945a4f";
+    private static final String PUBNUB_PUB_KEY = "pub-c-8ae56e53-70e7-4898-9595-2f8a57ba364a";
+
     private static PubNub pubNub;
     @Override
     public void onCreate() {
         super.onCreate();
 
         PNConfiguration pnConfiguration = new PNConfiguration();
-        pnConfiguration.setSubscribeKey("sub-c-2e7ff474-5863-11e5-b018-0619f8945a4f");
-        pnConfiguration.setPublishKey("pub-c-8ae56e53-70e7-4898-9595-2f8a57ba364a");
-        //pnConfiguration.setSecretKey("sec-c-YjczYmYxYzYtODM4Ny00NWQ4LTljNzAtMjg3NTZjNDYxNDUw");
+        pnConfiguration.setSubscribeKey(PUBNUB_SUB_KEY);
+        pnConfiguration.setPublishKey(PUBNUB_PUB_KEY);
+
+        // production keys when ready
+        /*
+          publish key: pub-c-8ae56e53-70e7-4898-9595-2f8a57ba364a
+          subscribe key: sub-c-2e7ff474-5863-11e5-b018-0619f8945a4f
+          channel: IOTHack
+         */
 
         pubNub = new PubNub(pnConfiguration);
     }
 
-    public static PubNub getPubNub() {
+    public static PubNub getPubNubObject() {
         return pubNub;
+    }
+
+    public static String getPubnubChannel() {
+        return PUBNUB_CHANNEL;
     }
 }
